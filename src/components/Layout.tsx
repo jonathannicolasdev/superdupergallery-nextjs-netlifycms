7601de5b00bc5d41e9ae7f85b801897b299edd9e
M Haidar Hanif <git@mhaidarhanif.com>
2021-10-02 23:38:20 +0700

    style: favicons

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -21,9 +21,6 @@ export default function Layout({ children }: Props) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-        <meta name="theme-color" content="#fff" />
       </Head>
 
       {/* <nav>
