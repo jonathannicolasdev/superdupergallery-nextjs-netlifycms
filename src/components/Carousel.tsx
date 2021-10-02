d40535aa5f478bd3e059c62ac242d35a1fd3d0c1
M Haidar Hanif <git@mhaidarhanif.com>
2021-10-03 00:28:24 +0700

    feat: carousel images

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,11 +1,59 @@
+import ReactMultiCarousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+
+import { styled } from '../../stitches.config'
+
 type Props = {
   count?: number
+  images: any
 }
 
-export default function Carousel({ count }: Props) {
+const CarouselItem = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+})
+
+const CarouselImage = styled('img', {
+  maxHeight: '500px',
+})
+
+export default function Carousel({ count, images }: Props) {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  }
+
   return (
-    <div>
-      <h1>Carousel {count}</h1>
-    </div>
+    <ReactMultiCarousel
+      responsive={responsive}
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={3000}
+      keyBoardControl={true}
+      removeArrowOnDeviceType={['tablet', 'mobile']}
+      showDots={true}
+    >
+      {images.map((imageURL, index) => {
+        return (
+          <CarouselItem key={index}>
+            <CarouselImage src={imageURL} alt={imageURL} />
+          </CarouselItem>
+        )
+      })}
+    </ReactMultiCarousel>
   )
 }
