ad0779749a946dfa59b52fe01efae5db79994560
M Haidar Hanif <git@mhaidarhanif.com>
2021-10-08 22:05:15 +0700

    fix: artist tag props

--- src/components/Artist.tsx
+++ src/components/Artist.tsx
@@ -1,3 +1,4 @@
+import { AnySrvRecord } from 'dns'
 import { styled } from '../../stitches.config'
 
 export const ArtistTags = ({ children }) => {
@@ -10,25 +11,30 @@ export const ArtistTags = ({ children }) => {
   return <Tags>{children}</Tags>
 }
 
-export const ArtistTag = ({ color = 'white', children }) => {
-  const Tag = styled('div', {
-    color: 'white',
-    padding: '0.5em',
-    margin: '0.25em',
-    border: '2px solid white',
-    variants: {
-      color: {
-        white: { borderColor: 'white' },
-        red: { borderColor: 'red' },
-        orange: { borderColor: 'orange' },
-        yellow: { borderColor: 'yellow' },
-        green: { borderColor: 'green' },
-        blue: { borderColor: 'blue' },
-        purple: { borderColor: 'purple' },
-        pink: { borderColor: 'pink' },
-      },
-    },
-  })
+type ArtistTagProps = {
+  color?: any
+  children: React.ReactNode
+}
 
+const Tag = styled('div', {
+  color: 'white',
+  padding: '0.5em',
+  margin: '0.25em',
+  border: '2px solid white',
+  variants: {
+    color: {
+      white: { borderColor: 'white' },
+      red: { borderColor: 'red' },
+      orange: { borderColor: 'orange' },
+      yellow: { borderColor: 'yellow' },
+      green: { borderColor: 'green' },
+      blue: { borderColor: 'blue' },
+      purple: { borderColor: 'purple' },
+      pink: { borderColor: 'pink' },
+    },
+  },
+})
+
+export const ArtistTag = ({ color, children }: ArtistTagProps) => {
   return <Tag color={color}>{children}</Tag>
 }
