609fe151f26d20dca6676ca61518730fd2501af6
M Haidar Hanif <git@mhaidarhanif.com>
2021-10-08 20:40:21 +0700

    style: carousel on mobile

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -18,20 +18,12 @@ const CarouselImage = styled('img', {
 
 export default function Carousel({ images }: Props) {
   const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 1,
-    },
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 1,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
+      breakpoint: { max: 3000, min: 768 },
       items: 1,
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 768, min: 0 },
       items: 1,
     },
   }
