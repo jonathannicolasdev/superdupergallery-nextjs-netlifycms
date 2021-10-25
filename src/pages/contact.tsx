1d7e0845acaf99be0e6e830c0b2363c0c942f0d1
M Haidar Hanif <git@mhaidarhanif.com>
2021-10-25 22:48:41 +0700

    feat: contact subscribe use mailchimp

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,4 +1,6 @@
 import NextHead from 'next/head'
+import MailchimpSubscribe from 'react-mailchimp-subscribe'
+import { styled } from '../../stitches.config'
 
 import Layout from '../components/Layout'
 import BasicMeta from '../components/meta/BasicMeta'
@@ -6,6 +8,27 @@ import OpenGraphMeta from '../components/meta/OpenGraphMeta'
 import TwitterCardMeta from '../components/meta/TwitterCardMeta'
 import Hero from '../components/Hero'
 import AnimatedHeading from '../components/AnimatedHeading'
+import { Center } from '../components/Content'
+
+const url =
+  'https://superdupergallery.us5.list-manage.com/subscribe/post?u=ea7f4eb41aea3e44277132474&amp;id=4324c31b57'
+
+const SimpleForm = () => <MailchimpSubscribe url={url} />
+
+const FormWrapper = styled('div', {
+  input: {
+    padding: '1em',
+    fontSize: '1rem',
+  },
+  button: {
+    padding: '1em',
+    fontSize: '1rem',
+    color: 'white',
+    cursor: 'pointer',
+    border: '2px solid white',
+    background: 'none',
+  },
+})
 
 export default function AboutPage() {
   return (
@@ -21,6 +44,12 @@ export default function AboutPage() {
       <Hero>
         <AnimatedHeading sentence="Contact Us" />
       </Hero>
+
+      <Center>
+        <FormWrapper>
+          <SimpleForm />
+        </FormWrapper>
+      </Center>
     </Layout>
   )
 }
